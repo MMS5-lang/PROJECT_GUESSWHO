3f823442cc9c286c63b437d9b2afa202039e29e7
MMS5-lang <miloszmattras@gmail.com>
2026-03-19 18:15:41 +0100

    liczniki

--- rtl/vga_timing.sv
+++ rtl/vga_timing.sv
@@ -24,18 +24,37 @@ module vga_timing (
     import vga_pkg::*;
 
 
-    /**
-     * Local variables and signals
-     */
+/* Local variables and signals */
 
-    // Add your signals and variables here.
-
-
-    /**
-     * Internal logic
-     */
-
-    // Add your code here.
-
+    logic [10:0] vcount_nxt, hcount_nxt;
+    logic vsync_nxt, vblnk_nxt, hsync_nxt, hblnk_nxt;
+    
+    /* Internal logic */
+    
+    always_ff @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            vcount <= 'b0;
+            hcount<= 'b0;            
+        end else begin
+            vcount <= vcount_nxt;
+            hcount <= hcount_nxt;
+        end
+    end
+    
+    always_comb begin
+        vcount_nxt = vcount;
+        hcount_nxt = hcount;
+            if (hcount == HOR_TOTAL_TIME - 1) begin
+                hcount_nxt = '0;
+                if (vcount == VER_TOTAL_TIME - 1) begin
+                    vcount_nxt = '0;
+                end else begin
+                    vcount_nxt = vcount + 1;
+                end
+            end else begin
+                hcount_nxt = hcount + 1;
+            end
+        end
+    
 
 endmodule
