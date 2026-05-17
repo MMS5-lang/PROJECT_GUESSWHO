a776dbeabc66a34f28cbce4572ea65b985d04f39
KarolinaMatyszkowicz <karolamatyszkowicz@gmail.com>
2026-05-18 00:11:35 +0200

    dodanie pejsow

--- rtl/render/face_renderer.sv
+++ rtl/render/face_renderer.sv
@@ -32,22 +32,27 @@ localparam int HAIR_1_H = 64;
 localparam int HAIR_1_X_OFF = 4; 
 localparam int HAIR_1_Y_OFF = 27;
 
-
+localparam int PAYOT_W = 104;   
+localparam int PAYOT_H = 60;  
+localparam int PAYOT_X_OFF = 14; 
+localparam int PAYOT_Y_OFF = 105;
 
 logic [11:0] head_rom [0:HEAD_W*HEAD_H-1];
 logic [11:0] glasses_rom [0:GLASSES_W*GLASSES_H-1];
 logic [11:0] hair_1_rom [0:HAIR_1_W*HAIR_1_H-1];
+logic [11:0] payot_rom [0:PAYOT_W*PAYOT_H-1];
 
 initial begin
     $readmemh("../../head_shape.dat", head_rom);
     $readmemh("../../glasses.dat", glasses_rom);
     $readmemh("../../hair_1.dat", hair_1_rom);
+    $readmemh("../../payot.dat", payot_rom);
 end
 
-// Format 4-bitowy: [7] wlosy 1, [5] kolor wlosow (blond brunet) [4] kolor oczu (niebieskie, zielone), [3] Kolor skóry (bialy, czarny), [2] Okulary (tak, nie), [1] Czapka(tak, nie), [0] Broda (tak, nie)
+// Format 4-bitowy: [7] wlosy 1, [5] kolor wlosow (blond brunet) [4] kolor oczu (niebieskie, zielone), [3] Kolor skóry (bialy, czarny), [2] Okulary (tak, nie), [1] Czapka(tak, nie), [0] pejsy (tak, nie)
 localparam logic [6:0] CHAR_TRAITS [0:17] = '{
     7'b1000000, 7'b1100001, 7'b0100010, 7'b0010011, 7'b0110100, 7'b1000101,
-    7'b0110110, 7'b1100111, 7'b0001000, 7'b0111001, 7'b1111010, 7'b1001011, 
+    7'b0100111, 7'b1100111, 7'b0001000, 7'b0111001, 7'b1111010, 7'b1001011, 
     7'b1001100, 7'b1111001, 7'b0101110, 7'b1011111, 7'b0101001, 7'b1110001  
 };
 
@@ -71,6 +76,9 @@ logic [10:0] hair_1_lx, hair_1_ly;
 logic [11:0] hair_1_pixel;
 logic in_hair_1_area;
 
+logic [10:0] payot_lx, payot_ly;
+logic [11:0] payot_pixel;
+logic in_payot_area;
 
 
 always_ff @(posedge clk or negedge rst_n) begin
@@ -106,7 +114,7 @@ always_comb begin
     cell_y = rel_y % CELL_H;
 
 
-    char_idx = (row * 7) + col;
+    char_idx = (row * 6) + col;
     if (char_idx > 17) char_idx = 17; 
     my_traits = CHAR_TRAITS[char_idx];
 
@@ -131,6 +139,15 @@ always_comb begin
                                      (cell_x >= HAIR_1_X_OFF && cell_x < HAIR_1_X_OFF + HAIR_1_W) &&
                                      (cell_y >= HAIR_1_Y_OFF && cell_y < HAIR_1_Y_OFF + HAIR_1_H);
 
+    payot_lx = cell_x - PAYOT_X_OFF;
+    payot_ly = cell_y - PAYOT_Y_OFF;
+                   
+    in_payot_area = my_traits[0] && 
+                                     (cell_x >= PAYOT_X_OFF && cell_x < PAYOT_X_OFF + PAYOT_W) &&
+                                     (cell_y >= PAYOT_Y_OFF && cell_y < PAYOT_Y_OFF + PAYOT_H);
+
+
+
 
     if (in_head_area)
         head_pixel = head_rom[head_ly * HEAD_W + head_lx];
@@ -147,9 +164,14 @@ always_comb begin
     else 
         hair_1_pixel = 12'hf_0_f;
 
+    if (in_payot_area)
+        payot_pixel = payot_rom[payot_ly * PAYOT_W + payot_lx];
+    else
+        payot_pixel = 12'hf_0_f;
+
     if (in.vblnk || in.hblnk) begin
         rgb_nxt = 12'h0_0_0;
-        
+ 
    
     end else if (in_board && in.rgb == 12'hf_f_f) begin
     
@@ -160,9 +182,13 @@ always_comb begin
                 rgb_nxt = my_traits[5] ? 12'hd_b_7 : 12'h3_2_2; 
             else
                 rgb_nxt = hair_1_pixel;
-        end 
+        end else if (in_payot_area && payot_pixel != 12'hf_0_f) begin   
+            if (payot_pixel == 12'h3_2_2)
+                rgb_nxt = my_traits[5] ? 12'hd_b_7 : 12'h3_2_2; 
+            else
+                rgb_nxt = payot_pixel;
 
-        else if (in_head_area && head_pixel != 12'hf_0_f) begin
+        end else if (in_head_area && head_pixel != 12'hf_0_f) begin
             if (head_pixel == 12'hf_f_f) 
                 rgb_nxt = my_traits[3] ? 12'h8_5_2 : 12'hf_d_b; 
             else if (head_pixel == 12'h9_5_3)   
