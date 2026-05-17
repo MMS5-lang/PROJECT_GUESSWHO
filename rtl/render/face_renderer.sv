974244abe28e0454dc9dbbc1df72d8fe606a74d8
KarolinaMatyszkowicz <karolamatyszkowicz@gmail.com>
2026-05-17 23:03:45 +0200

    wlosy

--- rtl/render/face_renderer.sv
+++ rtl/render/face_renderer.sv
@@ -27,20 +27,28 @@ localparam int GLASSES_H = 24;
 localparam int GLASSES_X_OFF = 13; 
 localparam int GLASSES_Y_OFF = 92;
 
+localparam int HAIR_1_W = 132;   
+localparam int HAIR_1_H = 64;  
+localparam int HAIR_1_X_OFF = 4; 
+localparam int HAIR_1_Y_OFF = 27;
+
+
 
 logic [11:0] head_rom [0:HEAD_W*HEAD_H-1];
 logic [11:0] glasses_rom [0:GLASSES_W*GLASSES_H-1];
+logic [11:0] hair_1_rom [0:HAIR_1_W*HAIR_1_H-1];
 
 initial begin
     $readmemh("../../head_shape.dat", head_rom);
     $readmemh("../../glasses.dat", glasses_rom);
+    $readmemh("../../hair_1.dat", hair_1_rom);
 end
 
-// Format 4-bitowy: [4] kolor oczu (niebieskie, zielone), [3] Kolor skóry (bialy, czarny), [2] Okulary (tak, nie), [1] Czapka(tak, nie), [0] Broda (tak, nie)
-localparam logic [4:0] CHAR_TRAITS [0:17] = '{
-    5'b00000, 5'b00001, 5'b00010, 5'b10011, 5'b10100, 5'b00101,
-    5'b10110, 5'b00111, 5'b01000, 5'b11001, 5'b11010, 5'b01011, 
-    5'b01100, 5'b11101, 5'b01110, 5'b01111, 5'b10100, 5'b11000  
+// Format 4-bitowy: [7] wlosy 1, [5] kolor wlosow (blond brunet) [4] kolor oczu (niebieskie, zielone), [3] Kolor skóry (bialy, czarny), [2] Okulary (tak, nie), [1] Czapka(tak, nie), [0] Broda (tak, nie)
+localparam logic [6:0] CHAR_TRAITS [0:17] = '{
+    7'b1000000, 7'b1100001, 7'b0100010, 7'b0010011, 7'b0110100, 7'b1000101,
+    7'b0110110, 7'b1100111, 7'b0001000, 7'b0111001, 7'b1111010, 7'b1001011, 
+    7'b1001100, 7'b1111001, 7'b0101110, 7'b1011111, 7'b0101001, 7'b1110001  
 };
 
 logic in_board;
@@ -48,8 +56,8 @@ logic [10:0] rel_x, rel_y;
 logic [10:0] cell_x, cell_y;
 logic [2:0] row;
 logic [3:0] col;
-logic [4:0] char_idx;
-logic [4:0] my_traits;
+logic [6:0] char_idx;
+logic [6:0] my_traits;
 
 logic [10:0] head_lx, head_ly;
 logic [11:0] head_pixel;
@@ -59,8 +67,11 @@ logic [10:0] glasses_lx, glasses_ly;
 logic [11:0] glasses_pixel;
 logic in_glasses_area;
 
+logic [10:0] hair_1_lx, hair_1_ly;
+logic [11:0] hair_1_pixel;
+logic in_hair_1_area;
 
-logic is_hat, is_beard;
+
 
 always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
@@ -95,7 +106,7 @@ always_comb begin
     cell_y = rel_y % CELL_H;
 
 
-    char_idx = (row * 6) + col;
+    char_idx = (row * 7) + col;
     if (char_idx > 17) char_idx = 17; 
     my_traits = CHAR_TRAITS[char_idx];
 
@@ -108,10 +119,19 @@ always_comb begin
     glasses_lx = cell_x - GLASSES_X_OFF;
     glasses_ly = cell_y - GLASSES_Y_OFF;
                    
-                   // Ważne: Sprawdzamy, czy w obszarze okularów ORAZ czy postać w ogóle ma na nie "gen" (my_traits[2])
     in_glasses_area = my_traits[2] && 
                                      (cell_x >= GLASSES_X_OFF && cell_x < GLASSES_X_OFF + GLASSES_W) &&
                                      (cell_y >= GLASSES_Y_OFF && cell_y < GLASSES_Y_OFF + GLASSES_H);
+
+    hair_1_lx = cell_x - HAIR_1_X_OFF;
+    hair_1_ly = cell_y - HAIR_1_Y_OFF;
+                   
+
+    in_hair_1_area = my_traits[6] && 
+                                     (cell_x >= HAIR_1_X_OFF && cell_x < HAIR_1_X_OFF + HAIR_1_W) &&
+                                     (cell_y >= HAIR_1_Y_OFF && cell_y < HAIR_1_Y_OFF + HAIR_1_H);
+
+
     if (in_head_area)
         head_pixel = head_rom[head_ly * HEAD_W + head_lx];
     else
@@ -122,27 +142,34 @@ always_comb begin
     else
         glasses_pixel = 12'hf_0_f;
 
-    is_hat     = my_traits[1] && (cell_x >= 20 && cell_x < 120) && (cell_y >= 30 && cell_y < 60);
-    is_beard   = my_traits[0] && (cell_x >= 40 && cell_x < 100) && (cell_y >= 130 && cell_y < 160);
+    if (in_hair_1_area)
+        hair_1_pixel = hair_1_rom[hair_1_ly * HAIR_1_W + hair_1_lx];
+    else 
+        hair_1_pixel = 12'hf_0_f;
 
     if (in.vblnk || in.hblnk) begin
         rgb_nxt = 12'h0_0_0;
         
    
     end else if (in_board && in.rgb == 12'hf_f_f) begin
-        if (is_hat)          rgb_nxt = 12'h2_2_d;
-        else if (in_glasses_area && glasses_pixel != 12'hf_0_f) begin
+    
+        if (in_glasses_area && glasses_pixel != 12'hf_0_f) begin
             rgb_nxt = glasses_pixel;
-        end else if (is_beard)   rgb_nxt = 12'h5_2_0; 
+        end else if (in_hair_1_area && hair_1_pixel != 12'hf_0_f) begin   
+            if (hair_1_pixel == 12'h3_2_2)
+                rgb_nxt = my_traits[5] ? 12'hd_b_7 : 12'h3_2_2; 
+            else
+                rgb_nxt = hair_1_pixel;
+        end 
 
-    else if (in_head_area && head_pixel != 12'hf_0_f) begin
-        if (head_pixel == 12'hf_f_f) 
-            rgb_nxt = my_traits[3] ? 12'h8_5_2 : 12'hf_d_b; 
-        else if (head_pixel == 12'h9_5_3)   
-            rgb_nxt = my_traits[4] ? 12'h4_a_f : 12'h3_4_1; 
-        else
-            rgb_nxt = head_pixel;    
-            
+        else if (in_head_area && head_pixel != 12'hf_0_f) begin
+            if (head_pixel == 12'hf_f_f) 
+                rgb_nxt = my_traits[3] ? 12'h8_5_2 : 12'hf_d_b; 
+            else if (head_pixel == 12'h9_5_3)   
+                rgb_nxt = my_traits[4] ? 12'h4_a_f : 12'h3_4_1; 
+            else
+                rgb_nxt = head_pixel;    
+                
     end else begin
         rgb_nxt = in.rgb; 
     end
